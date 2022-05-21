73a6e300e95bff2b4dda6d5ed5c61ce036a32836
Timothy1102 <kuduthang834@gmail.com>
2022-05-21 20:25:34 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+import bodyParser from "body-parser";
+require("dotenv").config();
+
+let app = express();
+
+//config body-parser to post data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//config view engine
+configViewEngine(app);
+
+//init web routes
+initWebRoutes(app);
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Messenger bot is running at the port ${port}`);
+});
